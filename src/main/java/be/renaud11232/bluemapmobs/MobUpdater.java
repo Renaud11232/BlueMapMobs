12e64cbb0650e7e6caa56936ceb16d2ab4aac74f
Renaud11232 <gaspardrenaud@hotmail.com>
2024-03-20 16:07:54 +0100

    started implementing mob list computation

--- src/main/java/be/renaud11232/bluemapmobs/MobUpdater.java
+++ src/main/java/be/renaud11232/bluemapmobs/MobUpdater.java
@@ -1,26 +1,49 @@
 package be.renaud11232.bluemapmobs;
 
 import de.bluecolored.bluemap.api.BlueMapAPI;
-import de.bluecolored.bluemap.api.BlueMapWorld;
+import de.bluecolored.bluemap.api.markers.Marker;
+import de.bluecolored.bluemap.api.markers.MarkerSet;
+import de.bluecolored.bluemap.api.markers.POIMarker;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.LivingEntity;
+
+import java.util.*;
 
 public class MobUpdater implements Runnable {
 
     private final BlueMapAPI api;
+    private final Map<World, Map<LivingEntity, Marker>> worldMarkers;
 
     public MobUpdater(BlueMapAPI api) {
         this.api = api;
+        this.worldMarkers = new HashMap<>();
     }
 
     @Override
     public void run() {
         Bukkit.getServer().getWorlds().forEach(world -> {
-            BlueMapWorld blueMapWorld = api.getWorld(world).orElse(null);
-            if(blueMapWorld == null) {
-                return;
-            }
-            blueMapWorld.getMaps().forEach(map -> {
-
+            Set<LivingEntity> livingEntities = new HashSet<>(world.getLivingEntities());
+            Map<LivingEntity, Marker> markers = worldMarkers.computeIfAbsent(world, id -> new HashMap<>());
+            markers.entrySet().removeIf(entry -> !livingEntities.contains(entry.getKey()));
+            livingEntities.forEach(livingEntity -> {
+                Marker marker = markers.computeIfAbsent(livingEntity, id -> POIMarker.builder()
+                        .label(livingEntity.getName())
+                        .maxDistance(1000)
+                        .build()
+                );
+                marker.setPosition(livingEntity.getX(), livingEntity.getY(), livingEntity.getZ());
+            });
+            api.getWorld(world).ifPresent(blueMapWorld -> {
+                blueMapWorld.getMaps().forEach(map -> {
+                    MarkerSet markerSet = map.getMarkerSets().computeIfAbsent("mobs-markers", id -> MarkerSet.builder()
+                            .label("Mobs")
+                            .toggleable(true)
+                            .defaultHidden(false)
+                            .build()
+                    );
+                    markerSet.getMarkers().clear();
+                });
             });
         });
     }
