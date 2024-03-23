92c1608e23ec26cc46112cf082422f2231494e57
Renaud Gaspard <gaspardrenaud@hotmail.com>
2024-03-23 16:46:23 +0100

    plugin assets are now extracted on reload

--- src/main/java/be/renaud11232/bluemapmobs/BlueMapMobs.java
+++ src/main/java/be/renaud11232/bluemapmobs/BlueMapMobs.java
@@ -4,6 +4,12 @@ import de.bluecolored.bluemap.api.BlueMapAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.*;
+import java.util.stream.Stream;
+
+
 @SuppressWarnings("unused")
 public final class BlueMapMobs extends JavaPlugin {
 
@@ -14,6 +20,9 @@ public final class BlueMapMobs extends JavaPlugin {
             getLogger().info("Reloading configuration file");
             reloadConfig();
             getLogger().info("Configuration file reloaded");
+            getLogger().info("Copying assets");
+            extractAssets(api);
+            getLogger().info("Assets copied");
             getLogger().info("Scheduling task");
             Bukkit.getScheduler().runTaskTimer(this, new BlueMapMobsUpdateTask(api, getConfig()), 0, getConfig().getInt("general.refresh_rate", 20));
             getLogger().info("Task scheduled");
@@ -23,6 +32,28 @@ public final class BlueMapMobs extends JavaPlugin {
             Bukkit.getScheduler().cancelTasks(this);
             getLogger().info("Tasks cancelled");
         });
+    }
+
+    private void extractAssets(BlueMapAPI api) {
+        Path destinationPath = api.getWebApp().getWebRoot().resolve("assets").resolve(getClass().getName());
+        try {
+            Path jarPath = Path.of(getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+            try (FileSystem jar = FileSystems.newFileSystem(jarPath)) {
+                Path sourcePath = jar.getPath("assets");
+                try (Stream<Path> sourceFiles = Files.walk(sourcePath)) {
+                    sourceFiles.forEach(sourceFile -> {
+                        try {
+                            Files.copy(sourceFile, destinationPath.resolve(sourcePath.relativize(sourceFile).toString()), StandardCopyOption.REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+            }
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public static BlueMapMobs getInstance() {
