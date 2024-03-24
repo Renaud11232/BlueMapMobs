c9e80600608a785adc2e99115f1ef8f5addd6ddb
Renaud Gaspard <gaspardrenaud@hotmail.com>
2024-03-24 10:04:19 +0100

    added spawner minecart icon

--- src/main/java/be/renaud11232/bluemapmobs/vehicles/minecart/SpawnerMinecartMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/vehicles/minecart/SpawnerMinecartMarkerBuilder.java
@@ -1,8 +1,20 @@
 package be.renaud11232.bluemapmobs.vehicles.minecart;
 
+import be.renaud11232.bluemapmobs.Icon;
 import be.renaud11232.bluemapmobs.vehicles.VehicleEntityMarkerBuilder;
+import de.bluecolored.bluemap.api.markers.POIMarker;
 import org.bukkit.entity.minecart.SpawnerMinecart;
 
+import java.util.Optional;
+
 public class SpawnerMinecartMarkerBuilder implements VehicleEntityMarkerBuilder<SpawnerMinecart> {
+
+    @Override
+    public Optional<POIMarker> build(SpawnerMinecart spawnerMinecart) {
+        return VehicleEntityMarkerBuilder.super.build(spawnerMinecart).map(marker -> {
+            marker.setIcon(Icon.SPAWNER_MINECART.getPath(), Icon.SPAWNER_MINECART.getAnchor());
+            return marker;
+        });
+    }
 
 }
