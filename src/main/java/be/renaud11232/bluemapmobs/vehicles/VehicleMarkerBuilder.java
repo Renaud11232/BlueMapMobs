9a376a2864a064844a5ab043fe6c32270d9194e6
Renaud Gaspard <gaspardrenaud@hotmail.com>
2024-03-25 13:32:08 +0100

    used new switch statements

--- src/main/java/be/renaud11232/bluemapmobs/vehicles/VehicleMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/vehicles/VehicleMarkerBuilder.java
@@ -1,6 +1,8 @@
 package be.renaud11232.bluemapmobs.vehicles;
 
 import be.renaud11232.bluemapmobs.EntityMarkerBuilder;
+import be.renaud11232.bluemapmobs.vehicles.boat.BoatMarkerBuilder;
+import be.renaud11232.bluemapmobs.vehicles.minecart.MinecartMarkerBuilder;
 import org.bukkit.entity.Boat;
 import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Vehicle;
--- src/main/java/be/renaud11232/bluemapmobs/vehicles/boat/ChestBoatMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/vehicles/boat/ChestBoatMarkerBuilder.java
@@ -12,35 +12,18 @@ public class ChestBoatMarkerBuilder extends EntityMarkerBuilder<ChestBoat> {
     @Override
     public Optional<POIMarker> buildDefault(ChestBoat chestBoat) {
         return super.buildDefault(chestBoat).map(marker -> {
-            switch (chestBoat.getBoatType()) {
-                case ACACIA:
-                    marker.setIcon(Icon.ACACIA_CHEST_BOAT.getPath(), Icon.ACACIA_CHEST_BOAT.getAnchor());
-                    break;
-                case BAMBOO:
-                    marker.setIcon(Icon.BAMBOO_CHEST_RAFT.getPath(), Icon.BAMBOO_CHEST_RAFT.getAnchor());
-                    break;
-                case BIRCH:
-                    marker.setIcon(Icon.BIRCH_CHEST_BOAT.getPath(), Icon.BIRCH_CHEST_BOAT.getAnchor());
-                    break;
-                case CHERRY:
-                    marker.setIcon(Icon.CHERRY_CHEST_BOAT.getPath(), Icon.CHERRY_CHEST_BOAT.getAnchor());
-                    break;
-                case DARK_OAK:
-                    marker.setIcon(Icon.DARK_OAK_CHEST_BOAT.getPath(), Icon.DARK_OAK_CHEST_BOAT.getAnchor());
-                    break;
-                case JUNGLE:
-                    marker.setIcon(Icon.JUNGLE_CHEST_BOAT.getPath(), Icon.JUNGLE_CHEST_BOAT.getAnchor());
-                    break;
-                case MANGROVE:
-                    marker.setIcon(Icon.MANGROVE_CHEST_BOAT.getPath(), Icon.MANGROVE_CHEST_BOAT.getAnchor());
-                    break;
-                case OAK:
-                    marker.setIcon(Icon.OAK_CHEST_BOAT.getPath(), Icon.OAK_CHEST_BOAT.getAnchor());
-                    break;
-                case SPRUCE:
-                    marker.setIcon(Icon.SPRUCE_CHEST_BOAT.getPath(), Icon.SPRUCE_CHEST_BOAT.getAnchor());
-                    break;
-            }
+            Icon icon = switch (chestBoat.getBoatType()) {
+                case ACACIA -> Icon.ACACIA_CHEST_BOAT;
+                case BAMBOO -> Icon.BAMBOO_CHEST_RAFT;
+                case BIRCH -> Icon.BIRCH_CHEST_BOAT;
+                case CHERRY -> Icon.CHERRY_CHEST_BOAT;
+                case DARK_OAK -> Icon.DARK_OAK_CHEST_BOAT;
+                case JUNGLE -> Icon.JUNGLE_CHEST_BOAT;
+                case MANGROVE -> Icon.MANGROVE_CHEST_BOAT;
+                case OAK -> Icon.OAK_CHEST_BOAT;
+                case SPRUCE -> Icon.SPRUCE_CHEST_BOAT;
+            };
+            marker.setIcon(icon.getPath(), icon.getAnchor());
             return marker;
         });
     }
