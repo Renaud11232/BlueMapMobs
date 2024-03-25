9fd99b0b39af9b498a2667ac7c0b975c5acd1122
Renaud11232 <gaspardrenaud@hotmail.com>
2024-03-25 21:37:33 +0100

    added creatures impl

--- src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/CreatureMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/CreatureMarkerBuilder.java
@@ -1,10 +1,16 @@
 package be.renaud11232.bluemapmobs.markerbuilder.mob;
 
 import be.renaud11232.bluemapmobs.markerbuilder.MobEntityMarkerBuilder;
-import org.bukkit.entity.Creature;
+import be.renaud11232.bluemapmobs.markerbuilder.mob.creature.*;
+import org.bukkit.entity.*;
 
 public class CreatureMarkerBuilder extends MobEntityMarkerBuilder<Creature> {
     public CreatureMarkerBuilder() {
-        //TODO
+        registerMarkerBuilder(Ageable.class, new AgeableMarkerBuilder());
+        registerMarkerBuilder(Allay.class, new AllayMarkerBuilder());
+        registerMarkerBuilder(Golem.class, new GolemMarkerBuilder());
+        registerMarkerBuilder(Monster.class, new MonsterMarkerBuilder());
+        registerMarkerBuilder(NPC.class, new NPCMarkerBuilder());
+        registerMarkerBuilder(WaterMob.class, new WaterMobMarkerBuilder());
     }
 }
