26debf1cbedbaaa8ec417b251c40a9838ec2c94e
Renaud Gaspard <renaud.gaspard@etnic.be>
2024-03-26 10:29:59 +0100

    added npc marker builders

---
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/npc/abstractvillager/WanderingTraderMarkerBuilder.java
@@ -0,0 +1,15 @@
+package be.renaud11232.bluemapmobs.markerbuilder.mob.creature.npc.abstractvillager;
+
+import be.renaud11232.bluemapmobs.markerbuilder.MobEntityMarkerBuilder;
+import de.bluecolored.bluemap.api.markers.POIMarker;
+import org.bukkit.entity.WanderingTrader;
+
+import java.util.Optional;
+
+public class WanderingTraderMarkerBuilder extends MobEntityMarkerBuilder<WanderingTrader> {
+    @Override
+    public Optional<POIMarker> buildDefault(WanderingTrader mob) {
+        //TODO
+        return super.buildDefault(mob);
+    }
+}
