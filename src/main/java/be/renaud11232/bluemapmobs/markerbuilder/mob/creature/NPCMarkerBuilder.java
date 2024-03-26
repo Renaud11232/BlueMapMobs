f6fc1425ad37f1e952d55ea74642490c931dd6de
Renaud11232 <gaspardrenaud@hotmail.com>
2024-03-26 10:29:59 +0100

    added npc marker builders

--- src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/NPCMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/NPCMarkerBuilder.java
@@ -1,10 +1,12 @@
 package be.renaud11232.bluemapmobs.markerbuilder.mob.creature;
 
 import be.renaud11232.bluemapmobs.markerbuilder.MobEntityMarkerBuilder;
+import be.renaud11232.bluemapmobs.markerbuilder.mob.creature.npc.AbstractVillagerMarkerBuilder;
+import org.bukkit.entity.AbstractVillager;
 import org.bukkit.entity.NPC;
 
 public class NPCMarkerBuilder extends MobEntityMarkerBuilder<NPC> {
     public NPCMarkerBuilder() {
-        //TODO
+        registerMarkerBuilder(AbstractVillager.class, new AbstractVillagerMarkerBuilder());
     }
 }
--- src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/npc/AbstractVillagerMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/npc/AbstractVillagerMarkerBuilder.java
@@ -1,4 +1,15 @@
 package be.renaud11232.bluemapmobs.markerbuilder.mob.creature.npc;
 
-public class AbstractVillagerMarkerBuilder {
+import be.renaud11232.bluemapmobs.markerbuilder.MobEntityMarkerBuilder;
+import be.renaud11232.bluemapmobs.markerbuilder.mob.creature.npc.abstractvillager.VillagerMarkerBuilder;
+import be.renaud11232.bluemapmobs.markerbuilder.mob.creature.npc.abstractvillager.WanderingTraderMarkerBuilder;
+import org.bukkit.entity.AbstractVillager;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.WanderingTrader;
+
+public class AbstractVillagerMarkerBuilder extends MobEntityMarkerBuilder<AbstractVillager> {
+    public AbstractVillagerMarkerBuilder() {
+        registerMarkerBuilder(Villager.class, new VillagerMarkerBuilder());
+        registerMarkerBuilder(WanderingTrader.class, new WanderingTraderMarkerBuilder());
+    }
 }
