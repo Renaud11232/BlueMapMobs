0da4db91b883fc45ffdc6a65be4f1a49ef98323c
Renaud Gaspard <renaud.gaspard@etnic.be>
2024-03-26 11:17:38 +0100

    implemented all golem classes

--- src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/golem/IronGolemMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/golem/IronGolemMarkerBuilder.java
@@ -1,4 +1,15 @@
 package be.renaud11232.bluemapmobs.markerbuilder.mob.creature.golem;
 
-public class IronGolemMarkerBuilder {
+import be.renaud11232.bluemapmobs.markerbuilder.MobEntityMarkerBuilder;
+import de.bluecolored.bluemap.api.markers.POIMarker;
+import org.bukkit.entity.IronGolem;
+
+import java.util.Optional;
+
+public class IronGolemMarkerBuilder extends MobEntityMarkerBuilder<IronGolem> {
+    @Override
+    public Optional<POIMarker> buildDefault(IronGolem mob) {
+        //TODO
+        return super.buildDefault(mob);
+    }
 }
