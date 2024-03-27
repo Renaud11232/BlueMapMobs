6839780af3656bb80497cc70f9d70d696f12bbf5
Renaud11232 <gaspardrenaud@hotmail.com>
2024-03-27 08:26:52 +0100

    all entities are filtered through configuration

--- src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/animals/PigMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/animals/PigMarkerBuilder.java
@@ -18,4 +18,9 @@ public class PigMarkerBuilder extends MobEntityMarkerBuilder<Pig> {
         //TODO
         return super.buildDefault(mob);
     }
+
+    @Override
+    protected boolean isDisplayed() {
+        return getConfig().getBoolean("marker_sets.mobs.markers.types.pig", true);
+    }
 }
