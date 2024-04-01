de17dccb06833a28c1fc103b804585847c03fe14
Renaud Gaspard <gaspardrenaud@hotmail.com>
2024-04-01 15:41:54 +0200

    added rabbit colors

--- src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/animals/RabbitMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/animals/RabbitMarkerBuilder.java
@@ -15,9 +15,18 @@ public class RabbitMarkerBuilder extends MobEntityMarkerBuilder<Rabbit> {
     }
 
     @Override
-    public Optional<POIMarker> buildDefault(Rabbit mob) {
-        return super.buildDefault(mob).map(marker -> {
-            marker.setIcon(Icon.RABBIT.getPath(), Icon.RABBIT.getAnchor());
+    public Optional<POIMarker> buildDefault(Rabbit rabbit) {
+        return super.buildDefault(rabbit).map(marker -> {
+            Icon icon = rabbit.getName().equals("Toast") ? Icon.TOAST_RABBIT : switch (rabbit.getRabbitType()) {
+                case GOLD -> Icon.GOLD_RABBIT;
+                case WHITE -> Icon.WHITE_RABBIT;
+                case BLACK_AND_WHITE -> Icon.WHITE_SPLOTCHED_RABBIT;
+                case BLACK -> Icon.BLACK_RABBIT;
+                case BROWN -> Icon.BROWN_RABBIT;
+                case SALT_AND_PEPPER -> Icon.SALT_RABBIT;
+                case THE_KILLER_BUNNY -> Icon.CAERBANNOG_RABBIT;
+            };
+            marker.setIcon(icon.getPath(), icon.getAnchor());
             return marker;
         });
     }
