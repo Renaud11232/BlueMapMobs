8984b7917f5f62bb2fcea6207a1936044b658265
Renaud Gaspard <gaspardrenaud@hotmail.com>
2024-06-28 19:04:28 +0200

    using maps for selecting wolf icon

--- src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/animals/WolfMarkerBuilder.java
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/mob/creature/animals/WolfMarkerBuilder.java
@@ -6,12 +6,48 @@ import de.bluecolored.bluemap.api.markers.POIMarker;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Wolf;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class WolfMarkerBuilder extends MobEntityMarkerBuilder<Wolf> {
 
+    private final Map<Wolf.Variant, Icon> angryWolfIcons;
+    private final Map<Wolf.Variant, Icon> tamedWolfIcons;
+    private final Map<Wolf.Variant, Icon> untamedWolfIcons;
+
     public WolfMarkerBuilder(FileConfiguration config) {
         super(config);
+        angryWolfIcons = new HashMap<>();
+        angryWolfIcons.put(Wolf.Variant.ASHEN, Icon.ANGRY_ASHEN_WOLF);
+        angryWolfIcons.put(Wolf.Variant.BLACK, Icon.ANGRY_BLACK_WOLF);
+        angryWolfIcons.put(Wolf.Variant.CHESTNUT, Icon.ANGRY_CHESTNUT_WOLF);
+        angryWolfIcons.put(Wolf.Variant.PALE, Icon.ANGRY_PALE_WOLF);
+        angryWolfIcons.put(Wolf.Variant.RUSTY, Icon.ANGRY_RUSTY_WOLF);
+        angryWolfIcons.put(Wolf.Variant.SNOWY, Icon.ANGRY_SNOWY_WOLF);
+        angryWolfIcons.put(Wolf.Variant.SPOTTED, Icon.ANGRY_SPOTTED_WOLF);
+        angryWolfIcons.put(Wolf.Variant.STRIPED, Icon.ANGRY_STRIPED_WOLF);
+        angryWolfIcons.put(Wolf.Variant.WOODS, Icon.ANGRY_WOODS_WOLF);
+        tamedWolfIcons = new HashMap<>();
+        tamedWolfIcons.put(Wolf.Variant.ASHEN, Icon.TAMED_ASHEN_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.BLACK, Icon.TAMED_BLACK_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.CHESTNUT, Icon.TAMED_CHESTNUT_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.PALE, Icon.TAMED_PALE_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.RUSTY, Icon.TAMED_RUSTY_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.SNOWY, Icon.TAMED_SNOWY_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.SPOTTED, Icon.TAMED_SPOTTED_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.STRIPED, Icon.TAMED_STRIPED_WOLF);
+        tamedWolfIcons.put(Wolf.Variant.WOODS, Icon.TAMED_WOODS_WOLF);
+        untamedWolfIcons = new HashMap<>();
+        untamedWolfIcons.put(Wolf.Variant.ASHEN, Icon.UNTAMED_ASHEN_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.BLACK, Icon.UNTAMED_BLACK_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.CHESTNUT, Icon.UNTAMED_CHESTNUT_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.PALE, Icon.UNTAMED_PALE_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.RUSTY, Icon.UNTAMED_RUSTY_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.SNOWY, Icon.UNTAMED_SNOWY_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.SPOTTED, Icon.UNTAMED_SPOTTED_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.STRIPED, Icon.UNTAMED_STRIPED_WOLF);
+        untamedWolfIcons.put(Wolf.Variant.WOODS, Icon.UNTAMED_WOODS_WOLF);
     }
 
     @Override
@@ -19,71 +55,11 @@ public class WolfMarkerBuilder extends MobEntityMarkerBuilder<Wolf> {
         return super.buildDefault(wolf).map(marker -> {
             Icon icon;
             if(wolf.isAngry()) {
-                if (wolf.getVariant().equals(Wolf.Variant.ASHEN)) {
-                    icon = Icon.ANGRY_ASHEN_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.BLACK)) {
-                    icon = Icon.ANGRY_BLACK_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.CHESTNUT)) {
-                    icon = Icon.ANGRY_CHESTNUT_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.PALE)) {
-                    icon = Icon.ANGRY_PALE_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.RUSTY)) {
-                    icon = Icon.ANGRY_RUSTY_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.SNOWY)) {
-                    icon = Icon.ANGRY_SNOWY_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.SPOTTED)) {
-                    icon = Icon.ANGRY_SPOTTED_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.STRIPED)) {
-                    icon = Icon.ANGRY_STRIPED_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.WOODS)) {
-                    icon = Icon.ANGRY_WOODS_WOLF;
-                } else {
-                    icon = Icon.TAMED_WOLF;
-                }
+                icon = angryWolfIcons.getOrDefault(wolf.getVariant(), Icon.ANGRY_WOLF);
             } else if (wolf.isTamed()) {
-                if (wolf.getVariant().equals(Wolf.Variant.ASHEN)) {
-                    icon = Icon.TAMED_ASHEN_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.BLACK)) {
-                    icon = Icon.TAMED_BLACK_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.CHESTNUT)) {
-                    icon = Icon.TAMED_CHESTNUT_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.PALE)) {
-                    icon = Icon.TAMED_PALE_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.RUSTY)) {
-                    icon = Icon.TAMED_RUSTY_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.SNOWY)) {
-                    icon = Icon.TAMED_SNOWY_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.SPOTTED)) {
-                    icon = Icon.TAMED_SPOTTED_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.STRIPED)) {
-                    icon = Icon.TAMED_STRIPED_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.WOODS)) {
-                    icon = Icon.TAMED_WOODS_WOLF;
-                } else {
-                    icon = Icon.TAMED_WOLF;
-                }
+                icon = tamedWolfIcons.getOrDefault(wolf.getVariant(), Icon.TAMED_WOLF);
             } else {
-                if (wolf.getVariant().equals(Wolf.Variant.ASHEN)) {
-                    icon = Icon.UNTAMED_ASHEN_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.BLACK)) {
-                    icon = Icon.UNTAMED_BLACK_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.CHESTNUT)) {
-                    icon = Icon.UNTAMED_CHESTNUT_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.PALE)) {
-                    icon = Icon.UNTAMED_PALE_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.RUSTY)) {
-                    icon = Icon.UNTAMED_RUSTY_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.SNOWY)) {
-                    icon = Icon.UNTAMED_SNOWY_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.SPOTTED)) {
-                    icon = Icon.UNTAMED_SPOTTED_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.STRIPED)) {
-                    icon = Icon.UNTAMED_STRIPED_WOLF;
-                } else if (wolf.getVariant().equals(Wolf.Variant.WOODS)) {
-                    icon = Icon.UNTAMED_WOODS_WOLF;
-                } else {
-                    icon = Icon.UNTAMED_WOLF;
-                }
+                icon = untamedWolfIcons.getOrDefault(wolf.getVariant(), Icon.UNTAMED_WOLF);
             }
             marker.setIcon(icon.getPath(), icon.getAnchor());
             return marker;
