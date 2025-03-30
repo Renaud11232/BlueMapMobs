17548672d48a8fda43f45569f4091683326cca02
Renaud Gaspard <gaspardrenaud@hotmail.com>
2025-03-30 21:35:45 +0200

    yet another refactoring

---
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/v2/VariantMarkerBuilder.java
@@ -0,0 +1,9 @@
+package be.renaud11232.bluemapmobs.markerbuilder.v2;
+
+import be.renaud11232.bluemapmobs.registry.VariantIconRegistry;
+import org.bukkit.entity.Entity;
+
+public interface VariantMarkerBuilder<T extends Entity, V> extends MarkerBuilder<T> {
+    V getVariant(T entity);
+    void registerVariantIcons(VariantIconRegistry<V> registry);
+}
---
+++ src/main/java/be/renaud11232/bluemapmobs/markerbuilder/v2/VehicleEntityMarkerBuilder.java
@@ -0,0 +1,22 @@
+package be.renaud11232.bluemapmobs.markerbuilder.v2;
+
+import be.renaud11232.bluemapmobs.BlueMapMobsConfiguration;
+import de.bluecolored.bluemap.api.markers.POIMarker;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Vehicle;
+
+import java.util.Optional;
+
+public class VehicleEntityMarkerBuilder<T extends Vehicle> extends EntityMarkerBuilder<T> {
+    public VehicleEntityMarkerBuilder(FileConfiguration config, FileConfiguration defaultConfig) {
+        super(config, defaultConfig);
+    }
+
+    @Override
+    public Optional<POIMarker> build(T vehicle) {
+        return super.build(vehicle).map(marker -> {
+            marker.setMaxDistance(BlueMapMobsConfiguration.MarkerSets.Vehicles.Markers.MAX_DISTANCE.get(getConfig(), getDefaultConfig()));
+            return marker;
+        });
+    }
+}
