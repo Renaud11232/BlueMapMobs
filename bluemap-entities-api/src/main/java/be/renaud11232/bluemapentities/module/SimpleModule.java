202c455e6437886ff028f6a956b5898a2439371a
Renaud11232 <gaspardrenaud@hotmail.com>
2026-04-13 18:00:28 +0200

    small cleanup

--- bluemap-entities-api/src/main/java/be/renaud11232/bluemapentities/module/SimpleModule.java
+++ bluemap-entities-api/src/main/java/be/renaud11232/bluemapentities/module/SimpleModule.java
@@ -4,6 +4,7 @@ import be.renaud11232.bluemapentities.BlueMapEntitiesAPI;
 import be.renaud11232.bluemapentities.EntityConverter;
 import be.renaud11232.bluemapentities.markerbuilder.MarkerBuilder;
 import be.renaud11232.bluemapentities.entity.Entity;
+import be.renaud11232.bluemapentities.module.configuration.MarkerConfiguration;
 import be.renaud11232.bluemapentities.module.configuration.MarkerSetConfiguration;
 import be.renaud11232.bluemapentities.module.configuration.ModuleConfiguration;
 import de.bluecolored.bluemap.api.markers.Marker;
@@ -11,7 +12,6 @@ import de.bluecolored.bluemap.api.markers.MarkerSet;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public abstract class SimpleModule<SOURCE_TYPE, TARGET_TYPE extends Entity> implements Module {
     private final BlueMapEntitiesAPI api;
@@ -34,38 +34,33 @@ public abstract class SimpleModule<SOURCE_TYPE, TARGET_TYPE extends Entity> impl
                 .toList();
         Map<String, List<TARGET_TYPE>> entitiesByType = allEntities.stream()
                 .collect(Collectors.groupingBy(e -> e.getMarkerType().getName()));
-        //TODO: Clean this mess
         configuration.getMarkerSets()
                 .forEach(markerSetConfig -> {
-                    Map<String, Marker> markers = new HashMap<>();
-                    markerSetConfig.getMarkers()
+                    Map<String, Marker> markers = markerSetConfig.getMarkers()
                             .stream()
                             .filter(markerConfiguration -> markerConfiguration.getType().equals("*"))
                             .findAny()
-                            .map(Stream::of)
-                            .orElseGet(() -> markerSetConfig.getMarkers().stream())
-                            .forEach(markerConfiguration -> {
-                                List<TARGET_TYPE> entities;
-                                if (markerConfiguration.getType().equals("*")) {
-                                    entities = allEntities;
-                                } else {
-                                    entities = entitiesByType.getOrDefault(markerConfiguration.getType(), Collections.emptyList());
-                                }
-                                entities.forEach(entity -> markerBuilder.build(entity)
-                                        .map(marker -> {
-                                            marker.setMaxDistance(markerConfiguration.getMaxDistance());
-                                            return marker;
-                                        }).ifPresent(marker ->
-                                                markers.put(entity.getUUID().toString(), marker)
-                                        )
-                                );
-                            });
+                            .map(markerConfiguration -> buildMarkers(markerConfiguration, allEntities))
+                            .orElseGet(() -> markerSetConfig.getMarkers()
+                                    .stream()
+                                    .map(markerConfig -> buildMarkers(markerConfig, entitiesByType.getOrDefault(markerConfig.getType(), Collections.emptyList())))
+                                    .flatMap(map -> map.entrySet().stream())
+                                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                            );
                     api.getBlueMap().getWorld(world).ifPresent(w -> w.getMaps().forEach(map -> {
                         MarkerSet markerSet = map.getMarkerSets().computeIfAbsent(markerSetConfig.getId(), id -> buildMarkerSet(markerSetConfig));
                         markerSet.getMarkers().clear();
                         markerSet.getMarkers().putAll(markers);
                     }));
                 });
+    }
+
+    private Map<String, Marker> buildMarkers(MarkerConfiguration markerConfiguration, List<TARGET_TYPE> entities) {
+        return entities.stream()
+                .map(entity -> Map.entry(entity.getUUID().toString(), markerBuilder.build(entity)))
+                .filter(e -> e.getValue().isPresent())
+                .peek(e -> e.getValue().get().setMaxDistance(markerConfiguration.getMaxDistance()))
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().get()));
     }
 
     private MarkerSet buildMarkerSet(MarkerSetConfiguration conf) {
