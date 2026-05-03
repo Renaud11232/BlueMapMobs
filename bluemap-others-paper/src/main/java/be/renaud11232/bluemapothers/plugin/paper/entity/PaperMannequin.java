3c84f37f14490dc11b96c18ebb4fbc7030c82d5e
Renaud11232 <gaspardrenaud@hotmail.com>
2026-05-03 11:32:22 +0200

    Fixed mannequin head icons being placed too low

--- bluemap-others-paper/src/main/java/be/renaud11232/bluemapothers/plugin/paper/entity/PaperMannequin.java
+++ bluemap-others-paper/src/main/java/be/renaud11232/bluemapothers/plugin/paper/entity/PaperMannequin.java
@@ -1,9 +1,9 @@
 package be.renaud11232.bluemapothers.plugin.paper.entity;
 
+import be.renaud11232.bluemapothers.common.entity.BukkitLivingEntity;
 import be.renaud11232.bluemapothers.entity.Mannequin;
-import be.renaud11232.bluemapothers.common.entity.BukkitEntity;
 
-public class PaperMannequin extends BukkitEntity<org.bukkit.entity.Mannequin> implements Mannequin {
+public class PaperMannequin extends BukkitLivingEntity<org.bukkit.entity.Mannequin> implements Mannequin {
     public PaperMannequin(org.bukkit.entity.Mannequin wrapped) {
         super(wrapped);
     }
