7bc78f0d9e20c65e29d5738227c6bd8c2c270f91
Renaud11232 <gaspardrenaud@hotmail.com>
2026-06-24 20:52:06 +0200

    fixed plugins not loading on bukkit based servers

--- bluemap-entities-bukkit/src/main/java/be/renaud11232/bluemapentities/bukkit/BukkitConfiguration.java
+++ bluemap-entities-bukkit/src/main/java/be/renaud11232/bluemapentities/bukkit/BukkitConfiguration.java
@@ -4,7 +4,7 @@ import be.renaud11232.bluemapentities.Configuration;
 import be.renaud11232.bluemapentities.GeneralConfiguration;
 
 import java.util.Map;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class BukkitConfiguration implements Configuration {
     private final GeneralConfiguration general;
@@ -18,11 +18,12 @@ public class BukkitConfiguration implements Configuration {
         return general == null ? new BukkitGeneralConfiguration() : general;
     }
 
-    @SuppressWarnings("unchecked")
     public static BukkitConfiguration deserialize(Map<String, Object> args) {
-        BukkitGeneralConfiguration general = Optional.ofNullable((Map<String, Object>) args.get("general"))
-                .map(BukkitGeneralConfiguration::deserialize)
-                .orElse(null);
+        Map<String, Object> generalValues = args.entrySet()
+                .stream()
+                .filter(entry -> entry.getKey().startsWith("general."))
+                .collect(Collectors.toMap(entry -> entry.getKey().replace("general.", ""), Map.Entry::getValue));
+        BukkitGeneralConfiguration general = BukkitGeneralConfiguration.deserialize(generalValues);
         return new BukkitConfiguration(general);
     }
 }
