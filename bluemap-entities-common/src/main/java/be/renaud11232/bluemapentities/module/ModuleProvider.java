5d91e790f800dbfaf15a4abe06fcffc6e9f56496
Renaud11232 <gaspardrenaud@hotmail.com>
2026-08-02 11:42:48 +0200

    remove unused import

--- bluemap-entities-common/src/main/java/be/renaud11232/bluemapentities/module/ModuleProvider.java
+++ bluemap-entities-common/src/main/java/be/renaud11232/bluemapentities/module/ModuleProvider.java
@@ -1,7 +1,6 @@
 package be.renaud11232.bluemapentities.module;
 
 import be.renaud11232.bluemapentities.configuration.Configuration;
-import be.renaud11232.bluemapentities.entity.Entity;
 import de.bluecolored.bluemap.api.BlueMapAPI;
 
 public interface ModuleProvider<WORLD_TYPE, SOURCE_ENTITY_TYPE> {
